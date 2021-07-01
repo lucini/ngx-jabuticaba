b8b3dc6da0ec4982790880a2d7d52a982154ab48
Rafael Lucini <rafaellucini@gmail.com>
2021-06-30 23:00:25 -0300

    First commit

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  projects: ['<rootDir>/apps/ngx-jabuticaba-docs'],
+  projects: [
+    '<rootDir>/apps/ngx-jabuticaba-docs',
+    '<rootDir>/libs/ngx-jabuticaba',
+  ],
 };
---
+++ libs/ngx-jabuticaba/README.md
@@ -0,0 +1,7 @@
+# ngx-jabuticaba
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test ngx-jabuticaba` to execute the unit tests.
